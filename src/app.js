e05615a65deff66526edb7303e847cb4be0c2eda
Dave Seidman <daveseidman@Daves-MacBook-Pro.local>
2024-10-07 10:24:00 -0400

    adding cors

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const http = require('http'); // To create a server for Socket.IO
 const { Server } = require('socket.io');
 const { SerialPort, ReadlineParser } = require('serialport');
+const cors = require('cors'); // Require the CORS module
+
 const Theme = require('./models/Theme'); // Import the Theme model
 
 // Routes
@@ -19,13 +21,19 @@ const s3Routes = require('./routes/S3');
 const app = express();
 app.use(express.json()); // Middleware to parse JSON
 
+// Use CORS middleware
+app.use(cors({
+  origin: ['http://localhost:8080', 'https://daveseidman.github.io'],
+  methods: ['GET', 'POST'],
+}));
+
 // Create HTTP server
 const server = http.createServer(app);
 
 // Setup Socket.IO
 const io = new Server(server, {
   cors: {
-    origin: '*', // Allow all origins for development
+    origin: ['http://localhost:8080', 'https://daveseidman.github.io'],
     methods: ['GET', 'POST'],
   },
 });
@@ -44,6 +52,10 @@ app.use('/api/themes', themeRoutes);
 app.use('/api/data', dataRoutes);
 app.use('/api/s3', s3Routes);
 
+app.get('/', (req, res) => {
+  res.send('shokoe lab api');
+});
+
 // Handle Socket.IO connections
 io.on('connection', (socket) => {
   console.log('New client connected:', socket.id);
@@ -61,41 +73,5 @@ io.on('connection', (socket) => {
     console.log('Client disconnected:', socket.id);
   });
 });
-
-app.get('/', (req, res) => {
-  res.send('shokoe lab api');
-});
-
-// const port = new SerialPort({
-//   path: process.env.RFID_READER,
-//   baudRate: 9600,
-// });
-
-// const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
-
-// parser.on('data', async (data) => {
-//   console.log('RFID Data:', data);
-//   try {
-//     const theme = await Theme.findOne({ rfid: data.trim() }); // Trim the data to remove extra spaces or newline characters
-//     if (theme) {
-//       console.log(`Theme found: ${theme._id}`);
-//       io.emit('themeId', { theme: theme._id });
-//     } else {
-//       console.log('No theme found for RFID:', data);
-//       io.emit('themeId', { theme: null }); // Emit null if no theme is found
-//     }
-//   } catch (err) {
-//     console.error('Error looking up theme:', err);
-//     io.emit('error', 'Error looking up theme');
-//   }
-// });
-
-// port.on('open', () => {
-//   console.log('Serial Port Opened');
-// });
-
-// port.on('error', (err) => {
-//   console.error('Error: ', err.message);
-// });
 
 server.listen(process.env.PORT, () => console.log('Server running'));
